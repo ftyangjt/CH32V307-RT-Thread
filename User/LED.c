f47661c1eef66174328e87a42b7b299fd4078130
L-Yilang <wydnm060722@163.com>
2025-07-08 13:57:06 +0800

    aa

--- User/LED.c
+++ User/LED.c
@@ -39,7 +39,7 @@ static void breathing_thread_entry()
             breathing_config.b = 255;
             breathing_config.max_brightness = 100;
             breathing_config.cycle_time_ms = 3000;
-            run_time = - 5000;
+            run_time = 0;
         }
         if(g_light == 2){
             breathing_config.r = 255;
@@ -47,6 +47,7 @@ static void breathing_thread_entry()
             breathing_config.b = 150;
             breathing_config.max_brightness = 100;
             breathing_config.cycle_time_ms = 1350;
+            run_time = 5000;
         }        
         if(g_light == 3){
             breathing_config.r = 255;
@@ -66,7 +67,7 @@ static void breathing_thread_entry()
         float step = 2 * PI / (breathing_config.cycle_time_ms / 20.0f); // 每20ms更新一次，计算步长
 
         rt_kprintf("灯光线程启动\n");
-        while (run_time < 1000 * 5) { // 运行5秒
+        while (run_time < 1000 * 10) { // 运行5秒
             // 使用正弦波计算当前亮度百分比(0-max_brightness)
             brightness_factor = (sinf(angle) + 1.0f) / 2.0f;  // 将-1到1的正弦值映射到0到1
             current_brightness = (uint8_t)(breathing_config.max_brightness * brightness_factor);
